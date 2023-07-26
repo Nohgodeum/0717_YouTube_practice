2d084faf7c9303a54086827a13c3ac07e15cc078
Nohgodeum <78678573+Nohgodeum@users.noreply.github.com>
2023-07-27 00:22:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,46 +7,46 @@ import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return 'No file uploaded', 400
-        file = request.files['file']
-        image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
-        resized_image = resize_image(image, 350, 450)
-        _, original_image_buf = cv2.imencode('.jpg', resized_image)
-        transformed_image = random_color_transform(resized_image)
-        resized_transformed_image = resize_image(transformed_image, 350, 450)
-        _, transformed_image_buf = cv2.imencode('.jpg', resized_transformed_image)
+# @app.route('/', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         if 'file' not in request.files:
+#             return 'No file uploaded', 400
+#         file = request.files['file']
+#         image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
+#         resized_image = resize_image(image, 350, 450)
+#         _, original_image_buf = cv2.imencode('.jpg', resized_image)
+#         transformed_image = random_color_transform(resized_image)
+#         resized_transformed_image = resize_image(transformed_image, 350, 450)
+#         _, transformed_image_buf = cv2.imencode('.jpg', resized_transformed_image)
 
-        return render_template_string('''
-        <!doctype html>
-        <title>Images Comparison</title>
-        <h1>Images Comparison</h1>
-        <div style="display: flex;">
-          <div style="margin-right: 20px;">
-            <h2>Original Image</h2>
-            <img src="data:image/jpeg;base64,{{ original_image_base64 }}" alt="Original Image">
-          </div>
-          <div>
-            <h2>Transformed Image</h2>
-            <img src="data:image/jpeg;base64,{{ transformed_image_base64 }}" alt="Transformed Image">
-          </div>
-        </div>
-        ''', original_image_base64=base64.b64encode(original_image_buf).decode('utf-8'),
-             transformed_image_base64=base64.b64encode(transformed_image_buf).decode('utf-8'))
-    return '''
-    <!doctype html>
-    <title>Upload Image</title>
-    <h1>Upload Image</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-@app.route('/some/endpoint')
-def my_endpoint():
+#         return render_template_string('''
+#         <!doctype html>
+#         <title>Images Comparison</title>
+#         <h1>Images Comparison</h1>
+#         <div style="display: flex;">
+#           <div style="margin-right: 20px;">
+#             <h2>Original Image</h2>
+#             <img src="data:image/jpeg;base64,{{ original_image_base64 }}" alt="Original Image">
+#           </div>
+#           <div>
+#             <h2>Transformed Image</h2>
+#             <img src="data:image/jpeg;base64,{{ transformed_image_base64 }}" alt="Transformed Image">
+#           </div>
+#         </div>
+#         ''', original_image_base64=base64.b64encode(original_image_buf).decode('utf-8'),
+#              transformed_image_base64=base64.b64encode(transformed_image_buf).decode('utf-8'))
+#     return '''
+#     <!doctype html>
+#     <title>Upload Image</title>
+#     <h1>Upload Image</h1>
+#     <form method=post enctype=multipart/form-data>
+#       <input type=file name=file>
+#       <input type=submit value=Upload>
+#     </form>
+#     '''
+@app.route('/')
+def index():
     return "Hello, World!"
 
 
